1fe8f1eaf910b181cb4c487628376ae1abb724b4
Michael Bullet <craquebullet@gmail.com>
2024-12-12 13:46:03 -0300

    Create ConcurrencyChallenge.java

---
+++ Dia 26-30: Concorrência/ConcurrencyChallenge.java
@@ -0,0 +1,11 @@
+// Desenvolva um sistema que demonstre:
+// 1. Threads
+// 2. Executors
+// 3. Sincronização
+// 4. CompletableFuture
+public class ConcurrencyChallenge {
+    // Implemente:
+    // - Processamento paralelo de tarefas
+    // - Gerenciamento de thread pool
+    // - Combinação de resultados assíncronos
+}
