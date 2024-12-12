9d4f6c9669a08f22caba1512f1205dd0202b41ed
Michael Bullet <craquebullet@gmail.com>
2024-12-12 13:48:34 -0300

    Create FileManagementSystem.java

---
+++ Dia 51-60: NIO.2/FileManagementSystem.java
@@ -0,0 +1,10 @@
+// Desenvolva um sistema de:
+// 1. Manipulação de arquivos
+// 2. Busca e filtro
+// 3. Processamento de diretórios
+public class FileManagementSystem {
+    // Implemente métodos para:
+    // - Busca de arquivos
+    // - Cópia
+    // - Análise de diretórios
+}
